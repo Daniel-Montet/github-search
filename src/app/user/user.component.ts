abd58869ceaf5a3a5da623a4b4af7126f3411862
Daniel Montet <dmontetproff@gmail.com>
2019-04-02 10:08:43 +0300

    change opacity

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -23,66 +23,39 @@ interface ApiResponse {
 })
 export class UserComponent implements OnInit {
   //github properties
-  image;
-  name;
-  email;
-  public_repos;
-  followers;
-  created_at;
-  repos_url;//
+  image:any;
+  name:any;
+  email:any;
+  public_repos:any;
+  followers:any;
+  created_at:any;
+  repos_url:any;
+  
+  //
 
-  username;
-  inputname;
-  repositoryname;
+  username:any;
+  inputname:any;
+  repositoryname:any;
+  inputrepository:any;
 
   // input(value:string){
   //   value=this.inputname
   //   return value
   // }
-  inputchoice: Search[];
-  choiceselected:number;
+  
   userrepository:any;
 
   constructor(public gH: GithubServiceService, private http: HttpClient, private authservice: AuthenticateService) { }
 
-checkchoice(){
 
-}
 
-  displayname() {
-
-    this.http.get<ApiResponse>(environment.apiUrl + environment.users + this.inputname + environment.accesstoken).subscribe(data => {
-      this.image = data.avatar_url;
-      this.name = data.name;
-      this.email = data.email;
-      this.public_repos = data.public_repos;
-      this.followers = data.followers;
-      this.created_at = data.created_at;
-      this.repos_url = data.repos_url;
-
-    })
-  }
-
-  // showrepo(){
-
-  // }
-
-  // getUser(){
-  //   return this.http.get<ApiResponse>(environment.apiUrl+environment.user+this.inputname+environment.accesstoken)
-  //  }
-  // production: false,
-  // apiUrl:"https://api.github.com/",
-  // users:"users/",
-  // repositories:"repositories",
-  // accesstoken:"?access_token=b5ac045230c461971af93fa5d3a2129d75961c94",
-  // repos:"/repos/"
-  // GET /users/:username/repos
-  //GET /user/repos
+  
   //https://api.github.com/repos/daniel-montet/portfolio?access_token=b5ac045230c461971af93fa5d3a2129d75961c94
-  //https://api.github.com/repos/Daniel-Montet/portfolio?access_token=b5ac045230c461971af93fa5d3a2129d75961c94"
 
-  getRepo() {
-    this.http.get<ApiResponse>(environment.apiUrl + environment.repos +this.inputname+'/portfolio'+environment.accesstoken).subscribe(data => {
+  //https://api.github.com/repos/Daniel-Montet/portfolio?access_token=043586e9c75ac64b8b0b4007fdc449d18a2d168c"
+
+  getrepo() {//get requested user repository--search through repos
+    this.http.get<ApiResponse>(environment.apiUrl + 'repos/' +'daniel-montet'+'/'+this.inputname+environment.accesstoken).subscribe(data => {
       this.image = data.avatar_url;
       this.name = data.name;
       this.email = data.email;
@@ -93,9 +66,22 @@ checkchoice(){
     })
   }
 
-  getuserrepos(){
+  getuserrepos(){///loops and gets user specific repositories
     this.http.get(environment.apiUrl + environment.users +this.inputname+environment.repos+environment.accesstoken).subscribe(data => {
       this.userrepository=data
+
+    })
+  }
+  //https://api.github.com/users/daniel-montet?access_token=043586e9c75ac64b8b0b4007fdc449d18a2d168c
+
+  getname(){
+    this.http.get<ApiResponse>(environment.apiUrl+environment.users+this.inputname+environment.accesstoken).subscribe(data => {
+      this.image = data.avatar_url;
+      this.name = data.name;
+      this.email = data.email;
+      this.public_repos = data.public_repos;
+      this.followers = data.followers;
+      this.created_at = data.created_at;
 
     })
   }
@@ -104,14 +90,8 @@ checkchoice(){
     this.authservice.logout()
   }
 
-  ngOnInit() {
+  ngOnInit() {}
 
-    // this.inputchoice=[
-    //   {id:1,name:"username"},
-    //   {id:2,name:"repository"}
-    // ];
-    // this.choiceselected=1;
-  }
 
 
 }
